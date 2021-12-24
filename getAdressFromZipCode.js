f85ffafe61e9da9c4395816fdcb057caa61330c5
go-tokunaga <go-tokunaga@cybozu.co.jp>
2021-12-24 17:33:01 +0900

    不要な処理を削除

--- getAdressFromZipCode.js
+++ getAdressFromZipCode.js
@@ -1,18 +1,15 @@
 (() => {
     'use strict';
 
-
     const eventsShowAndEdit = [
         'app.record.create.show',
         'app.record.edit.show'
     ];
     let button;
     kintone.events.on(eventsShowAndEdit, event => {
-        console.log('表示イベント発生');
         button = document.createElement('button');
         button.textContent = '住所取得';
         const space = kintone.app.record.getSpaceElement('zipCodeButtonSpace').appendChild(button);
-        console.log('表示イベント返却');
         return event;
     });
 
@@ -21,7 +18,6 @@
         'app.record.edit.change.zipCode'
     ];
     kintone.events.on(eventsZipCodeChange, event => {
-        console.log("変更イベント発生")
         const zipCode = event.record.zipCode.value;
         if (String(zipCode).length !== 7) {
             console.log("7文字ではないため終了");
@@ -32,28 +28,21 @@
             console.log(`zipCode : ${zipCode}`);
             const URL = 'https://zip-cloud.appspot.com/api/search?zipcode=' + zipCode;
             kintone.proxy(URL, 'GET', {}, {}).then(response => {
-                //const obj = JSON.parse(response);
                 console.log('response : ' + response);
                 const obj = JSON.parse(response[0]);
-                //const address = obj.results[0].address1 + obj.results[0].address2 + 3
                 console.log("都道府県 : " + obj.results[0].address1);
                 console.log("市区町村 : " + obj.results[0].address2);
                 const address = obj.results[0].address1 + obj.results[0].address2;
 
-                // eventオブジェクトに値を入れて return すれば
+                // eventオブジェクトに値を入れて return すれば良いと思ったが反映されず
                 // event.record['address'].value = String(address);
 
+                // kintone.events.on のイベントハンドラ内で kintone.app.record.get はできないと思ったができた。
                 const recordObj = kintone.app.record.get();
                 recordObj.record.address.value = address;
                 kintone.app.record.set(recordObj);
-
-                console.log("変更イベント返却")
                 return event;
             });
-            // console.log("終了1");
-            // return event;
         }
-        // console.log("終了2");
-        // return event;
     });
 })();
