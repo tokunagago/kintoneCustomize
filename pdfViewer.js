c1e8fef97044a638e047d98d8bf524cca7f15b4c
go-tokunaga <go-tokunaga@cybozu.co.jp>
2022-01-12 23:14:44 +0900

    PDFビューワー

--- pdfViewer.js
+++ pdfViewer.js
@@ -1,3 +1,31 @@
 (() => {
-    
+  'use strict';
+  kintone.events.on('app.record.detail.show', (event) => {
+    if (!event.record.pdf.value[0]) return;
+
+    const fileKey = event.record.pdf.value[0].fileKey;
+    const URL = 'https://`subdomain`.cybozu.com/k/v1/file.json?fileKey=' + fileKey;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', URL);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.responseType = 'blob';
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // const blob = new Blob([xhr.response]);
+        var url = window.URL || window.webkitURL;
+        // const blobUrl = url.createObjectURL(blob);
+        const blobUrl = url.createObjectURL(xhr.response);
+        const spaceElement = kintone.app.record.getSpaceElement('space');
+        const objectElement = document.createElement('object');
+        objectElement.data = blobUrl;
+        let preview = '<object data="' + blobUrl + '" type="application/pdf" width="100%" height="100%">';
+        preview += '</object>';
+        $(spaceElement).append(preview).css('height', '500');
+      } else {
+        console.log(xhr.response.Text);
+      }
+    };
+    xhr.send();
+  });
 })();
